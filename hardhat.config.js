3bf0a14dd98a881a47609b746f3be585af525eea
Mizanali Panjwani <mizanalip786@gmail.com>
2022-03-28 21:00:25 +0530

    Create basic smart contracts

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+require('@nomiclabs/hardhat-waffle');
+const fs = require('fs');
+const privateKey = fs.readFileSync('.secret').toString();
+const projectId = fs.readFileSync('.alchemyid').toString();
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: 'https://rpc-mainnet.matic.network',
+      accounts: [privateKey]
+    }
+  },
+  solidity: '0.8.4'
+};
