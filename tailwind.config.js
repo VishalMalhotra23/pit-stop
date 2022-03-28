95b6b1946f8a3d6eb6167d45936aad50c6c9b36f
Mizanali Panjwani <mizanalip786@gmail.com>
2022-03-29 02:31:10 +0530

    Tailwind config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  media: false, // or 'media' or 'class'
   theme: {
     colors: {
       background: '#1F1F1F',
