eccafb3880b9dfb9dabc8c57c829901f237a7581
Mizanali Panjwani <mizanalip786@gmail.com>
2022-03-31 03:20:04 +0530

    Extract auth logic into hook

---
+++ hooks/useAuth.ts
@@ -0,0 +1,57 @@
+import { useCallback, useState } from 'react';
+import { ethers } from 'ethers';
+import Web3Modal from 'web3modal';
+import WalletConnectProvider from '@walletconnect/web3-provider';
+import { useDispatch } from 'react-redux';
+import { authSuccess } from '../store/auth/actions';
+
+export default function useAuth() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const getWeb3Modal = useCallback(async () => {
+    const web3Modal = new Web3Modal({
+      network: 'mainnet',
+      cacheProvider: false,
+      providerOptions: {
+        walletconnect: {
+          package: WalletConnectProvider,
+          options: {
+            infuraId: process.env.NEXT_PUBLIC_INFURA_ID
+          }
+        }
+      }
+    });
+    return web3Modal;
+  }, []);
+
+  const connect = useCallback(async () => {
+    const web3Modal = await getWeb3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const accounts = await provider.listAccounts();
+    signIn(connection, accounts[0]);
+  }, []);
+
+  const signIn = useCallback(async (connection: any, account: string) => {
+    const authData = await fetch(`/api/auth?address=${account}`);
+    const user = await authData.json();
+    console.log(user.nonce);
+    // @ts-ignore
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const signature = await signer.signMessage(user.nonce.toString());
+    const response = await fetch(
+      `/api/verify?address=${account}&signature=${signature}`
+    );
+    const data = await response.json();
+    setLoggedIn(data.authenticated);
+    console.log(loggedIn);
+    dispatch(authSuccess(account));
+  }, []);
+
+  return {
+    connect
+  };
+}
