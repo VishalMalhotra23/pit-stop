9e4755d9a98a9cd2b606bc3bf24186d7f5852d87
Mizanali Panjwani <mizanalip786@gmail.com>
2022-04-01 19:55:25 +0530

    Points system

--- pages/api/points.ts
+++ pages/api/points.ts
@@ -16,21 +16,23 @@ export default async function wager(
 
     const results = raceData.data.MRData.RaceTable.Races[0].Results;
 
-    let standings: any = [];
+    let points = 0;
 
     results.map((result: any) => {
       let standing = {
         position: result.position,
-        points: result.points,
-        name: `${result.Driver.givenName} ${result.Driver.familyName}`
+        points: parseInt(result.points),
+        name: `${result.Driver.givenName.toLowerCase()}-${result.Driver.familyName.toLowerCase()}`
       };
 
-      standings.push(standing);
+      if (standing.name === 'nico-hülkenberg')
+        standing.name = 'nico-hulkenberg';
+      if (standing.name === 'sergio-pérez') standing.name = 'sergio-perez';
+
+      if (standing.name === driver) points = standing.points;
     });
 
-    console.log(standings);
-
-    res.status(200).json({ success: true, points: 100 });
+    res.status(200).json({ success: true, points });
   } catch (error) {
     res.status(400).json({ success: false });
   }
